3d98617e31c90b92081ce15d67852189b58b547a
Val <saviorand@gmail.com>
2024-09-30 19:52:59 +0200

    add default build environment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
-# The environment to build the package for. Usually "default", but might be "nightly" or others.
-ENVIRONMENT="${1-default}"
+
+# The environment to build the package for. Defaults to "default" if not provided.
+ENVIRONMENT="${1:-default}"
+
 if [[ "${ENVIRONMENT}" == "--help" ]]; then
     echo "Usage: ENVIRONMENT - Argument 1 corresponds with the environment you wish to build the package for."
     exit 0
 fi
+
+echo "Building for environment: ${ENVIRONMENT}"
+
 magic run template -m "${ENVIRONMENT}"
 magic run rattler-build build -r recipes -c https://conda.modular.com/max -c conda-forge --skip-existing=all
